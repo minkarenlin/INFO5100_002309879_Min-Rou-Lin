bd0466e4e4bbe95815b525b041b6fabd061110a5
min rou lin <lin.minr@northeastern.edu>
2025-03-28 20:49:11 -0700

    Add files via upload

---
+++ exercises/exercise8/demo/src/com/example/BookParser.java
@@ -0,0 +1,201 @@
+package com.example;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class BookParser {
+
+    // 方法：解析 XML 文件
+    public static void parseXML(String xmlFilePath) {
+        try {
+            File inputFile = new File(xmlFilePath);
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+
+            NodeList bookList = doc.getElementsByTagName("Book");
+            for (int i = 0; i < bookList.getLength(); i++) {
+                Node bookNode = bookList.item(i);
+                if (bookNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element bookElement = (Element) bookNode;
+                    String title = bookElement.getElementsByTagName("title").item(0).getTextContent();
+                    String publishedYear = bookElement.getElementsByTagName("publishedYear").item(0).getTextContent();
+                    String numberOfPages = bookElement.getElementsByTagName("numberOfPages").item(0).getTextContent();
+
+                    System.out.println("Title: " + title);
+                    System.out.println("Published Year: " + publishedYear);
+                    System.out.println("Number of Pages: " + numberOfPages);
+
+                    NodeList authorList = bookElement.getElementsByTagName("author");
+                    for (int j = 0; j < authorList.getLength(); j++) {
+                        System.out.println("Author: " + authorList.item(j).getTextContent());
+                    }
+                    System.out.println();
+                }
+            }
+        } catch (IOException | ParserConfigurationException | SAXException e) {
+        // Handle exceptions more specifically
+            System.err.println("Error occurred while processing the XML file: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    // 方法：解析 JSON 文件
+    public static void parseJSON(String jsonFilePath) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(jsonFilePath))) {
+            StringBuilder jsonContent = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                jsonContent.append(line);
+            }
+
+            JSONObject jsonObject = new JSONObject(jsonContent.toString());
+            JSONObject bookShelf = jsonObject.getJSONObject("BookShelf");
+            JSONArray books = bookShelf.getJSONArray("Books");
+
+            for (int i = 0; i < books.length(); i++) {
+                JSONObject book = books.getJSONObject(i);
+                String title = book.getString("title");
+                int publishedYear = book.getInt("publishedYear");
+                int numberOfPages = book.getInt("numberOfPages");
+
+                System.out.println("Title: " + title);
+                System.out.println("Published Year: " + publishedYear);
+                System.out.println("Number of Pages: " + numberOfPages);
+
+                JSONArray authors = book.getJSONArray("authors");
+                for (int j = 0; j < authors.length(); j++) {
+                    System.out.println("Author: " + authors.getString(j));
+                }
+                System.out.println();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 方法：向 XML 文件中添加新的图书
+    public static void addNewBookToXML(String xmlFilePath) {
+        try {
+            File inputFile = new File(xmlFilePath);
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+
+            Element bookshelf = (Element) doc.getElementsByTagName("BookShelf").item(0);
+
+            // 创建新的图书条目
+            Element newBook = doc.createElement("Book");
+
+            Element title = doc.createElement("title");
+            title.appendChild(doc.createTextNode("The Rising Sun"));
+            newBook.appendChild(title);
+
+            Element publishedYear = doc.createElement("publishedYear");
+            publishedYear.appendChild(doc.createTextNode("2023"));
+            newBook.appendChild(publishedYear);
+
+            Element numberOfPages = doc.createElement("numberOfPages");
+            numberOfPages.appendChild(doc.createTextNode("280"));
+            newBook.appendChild(numberOfPages);
+
+            Element authors = doc.createElement("authors");
+            Element author1 = doc.createElement("author");
+            author1.appendChild(doc.createTextNode("Alice Green"));
+            authors.appendChild(author1);
+            newBook.appendChild(authors);
+
+            bookshelf.appendChild(newBook);
+
+            // 保存更新后的 XML 文件
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File(xmlFilePath));
+            transformer.transform(source, result);
+
+            System.out.println("New Book added to XML!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 方法：向 JSON 文件中添加新的图书
+    public static void addNewBookToJSON(String jsonFilePath) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(jsonFilePath))) {
+            StringBuilder jsonContent = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                jsonContent.append(line);
+            }
+
+            JSONObject jsonObject = new JSONObject(jsonContent.toString());
+            JSONObject bookShelf = jsonObject.getJSONObject("BookShelf");
+            JSONArray books = bookShelf.getJSONArray("Books");
+
+            // 创建新的图书条目
+            JSONObject newBook = new JSONObject();
+            newBook.put("title", "The Rising Sun");
+            newBook.put("publishedYear", 2023);
+            newBook.put("numberOfPages", 280);
+            newBook.put("authors", new JSONArray().put("Alice Green"));
+
+            books.put(newBook);
+
+            // 保存更新后的 JSON 文件
+            try (FileWriter writer = new FileWriter(jsonFilePath)) {
+                writer.write(jsonObject.toString(4)); // 美化输出，带缩进
+            }
+
+            System.out.println("New Book added to JSON!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // main 方法：程序入口
+    public static void main(String[] args) {
+        String xmlFilePath = "resources/books.xml"; // XML 文件路径
+        String jsonFilePath =  "resources/books.json"; // JSON 文件路径
+
+        // 解析原始的 XML 和 JSON 文件
+        System.out.println("XML Parser Output:");
+        parseXML(xmlFilePath);
+
+        System.out.println("JSON Parser Output:");
+        parseJSON(jsonFilePath);
+
+        // 向 XML 和 JSON 文件中添加新图书
+        addNewBookToXML(xmlFilePath);
+        addNewBookToJSON(jsonFilePath);
+
+        // 重新解析并确认已添加的新图书
+        System.out.println("Reprinted XML after adding new book:");
+        parseXML(xmlFilePath);
+
+        System.out.println("Reprinted JSON after adding new book:");
+        parseJSON(jsonFilePath);
+    }
+}
